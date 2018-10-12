c2f7e6090369349ba6b1a970d70fa8e135f29629
sanlf <a215215573@alumnos.unison.mx>
2018-10-12 12:50:52 -0700

    Fix intersection of disks

--- WEB-Cech.scale.cpp
+++ WEB-Cech.scale.cpp
@@ -1,5 +1,5 @@
-/* The following script corresponds to the algororithm Cech.scale, 
- * which was established in the paper "A numerical approach for the filtered generalized Cech complex", 
+/* The following script corresponds to the algororithm Cech.scale,
+ * which was established in the paper "A numerical approach for the filtered generalized Cech complex",
  * by Jesus F. Espinoza, Rosalia Hernandez-Amador, Hector Alfredo Hernandez Hernandez and Beatriz Ramonetti Valencia.
  */
 
@@ -37,7 +37,7 @@ double rho(double M[3][3], int m, double lambda, double prec){
     int i, j, D, k, count, k_index;
     int rho_ij_is_higher, rho_ji_is_higher, double_intersection;
     int k_values[m-2];
-    
+
     double rho_value = -999999;
     double i_radious_reescaled, j_radious_reescaled, dist_ij, r_ij, a, b;
     double sum, distdkcenter; //auxiliar variables
@@ -47,41 +47,42 @@ double rho(double M[3][3], int m, double lambda, double prec){
     double k_center[2];
     double n_ij[2];
     double rho_ijk[m-2], rho_jik[m-2];
-   
+
     for(i=0;i<m-1;i++){
         for(j=i+1;j<m;j++){
             // Calculate the intersection points d_ij:
             vs i_center[D]=M[i][D]; i_radious_reescaled = lambda*M[i][2];
             vs j_center[D]=M[j][D]; j_radious_reescaled = lambda*M[j][2];
-            
+
             vs c_ij[D]=j_center[D]-i_center[D];
             sum=0; vs{ sum+=pow(i_center[D]-j_center[D],2);} dist_ij=sqrt(sum);
             double_intersection=0;
-            
+
             // Radii close enough, are equal_
             if(fabs(i_radious_reescaled-j_radious_reescaled) <= prec){
                 j_radious_reescaled = i_radious_reescaled;
             }
-            
+
             // Concentric disks
             if(dist_ij <= prec){
                 vs d_ij[D] = i_center[D];
             }else{
                 // External tangency
                 if(fabs(dist_ij - (i_radious_reescaled+j_radious_reescaled)) < prec){
-                    vs d_ij[D] = (j_radious_reescaled*i_center[D] + i_radious_reescaled*j_center[D])/(i_radious_reescaled+j_radious_reescaled);
+                    vs d_ij[D] = (M[j][2]*i_center[D] + M[i][2]*j_center[D])/(M[i][2] + M[j][2]);
                 }else{
                     // Complete contention
                     if((dist_ij > prec) && ( dist_ij <= fabs(i_radious_reescaled-j_radious_reescaled)+prec)){
                         r_ij = fabs(i_radious_reescaled-j_radious_reescaled);
-                        if(j_radious_reescaled > i_radious_reescaled){
-                            vs i_center[D] = j_center[D]; i_radious_reescaled = j_radious_reescaled; vs c_ij[D] = -c_ij[D];
+                        if(j_radious_reescaled < i_radious_reescaled){
+                            vs d_ij[D] = i_center[D] + i_radious_reescaled*c_ij[D]/r_ij;
+                        }else{
+                            vs d_ij[D] = j_center[D] - j_radious_reescaled*c_ij[D]/r_ij;
                         }
-                        vs d_ij[D] = i_center[D] + i_radious_reescaled*c_ij[D]/r_ij;
                     }else{
                         // Two intersection points
                         n_ij[0]=-c_ij[1]; n_ij[1]=c_ij[0];
-                        
+
                         // Calculate the coefficients a and b such that
                         // d_ij = i_center + a * c_ij + b * n_ij and d_ji = i_center + a * c_ij - b * n_ij
                         a = 0.5*(1 + (i_radious_reescaled-j_radious_reescaled)*(i_radious_reescaled+j_radious_reescaled)/pow(dist_ij,2));
@@ -92,7 +93,7 @@ double rho(double M[3][3], int m, double lambda, double prec){
                     }
                 }
             }
-            
+
             // Take k such that k != i,j.
             count = 0;
             for(D=0; D<m; D++){
@@ -101,43 +102,43 @@ double rho(double M[3][3], int m, double lambda, double prec){
                     count++;
                 }
             }
-            
+
             rho_ij_is_higher = 1; k_index = 0;
             while(rho_ij_is_higher==1 && k_index != m-2){
                 k = k_values[k_index];
                 vs k_center[D]=M[k][D];
-                
+
                 distdkcenter=0;  vs{ distdkcenter+=pow(d_ij[D]-k_center[D],2);} distdkcenter=sqrt(distdkcenter);
                 rho_ijk[k_index] = lambda*M[k][2]-distdkcenter;
-                
+
                 if(rho_ijk[k_index] < rho_value){
                     rho_ij_is_higher = 0;
                 }
                 k_index++;
             }
-            
+
             if(rho_ij_is_higher==1){
                 rho_value=rho_ijk[0];
                 for(D=1;D<m-2;D++) if(rho_ijk[D]<rho_value) rho_value=rho_ijk[D];
             }
-            
+
             if(double_intersection==1){
                 k_index = 0;
                 rho_ji_is_higher = 1;
-                
+
                 while(rho_ji_is_higher==1 && k_index != m-2){
                     k = k_values[k_index];
                     vs k_center[D]=M[k][D];
-                    
+
                     distdkcenter=0;  vs{ distdkcenter+=pow(d_ji[D]-k_center[D],2);} distdkcenter=sqrt(distdkcenter);
                     rho_jik[k_index] = lambda*M[k][2]-distdkcenter;
-                    
+
                     if(rho_jik[k_index] < rho_value){
                         rho_ji_is_higher = 0;
                     }
                     k_index++;
                 }
-                
+
                 if(rho_ji_is_higher==1){
                     rho_value=rho_jik[0];
                     for(D=1;D<m-2;D++) if(rho_jik[D]<rho_value) rho_value=rho_jik[D];
@@ -156,12 +157,12 @@ double bisection(double M[3][3], int m, double a, double b, int dig_prec, double
     double rho_M_a = rho(M, m, a, prec);
     double rho_M_b = rho(M, m, b, prec);
     int n, num_iter;
-    
+
     num_iter = ceil((log10(a*(sqrt(4.0/3)-1)) + dig_prec)/log10(2));
     for(n=1; n <= num_iter; n++){
         mp = 0.5*(a + b);
         rho_M_mp = rho(M, m, mp, prec);
-        
+
         if(fabs(rho_M_mp) <= prec){
             break;
         }else{
@@ -193,7 +194,7 @@ int main(){
     double dist_ij, r_ij, a, b, distdkcenter;
     double sum, sum21,sum22;
     double i_radious_reescaled, j_radious_reescaled, k_radious_reescaled, k_radious_reescaled2, radious1_reescaled, radious2_reescaled;
-    
+
     double edges_scale[maxm][maxm];
     double i_center[2], j_center[2], k_center[2];
     double c_ij[2], d_ij[2], d_ji[2], n_ij[2];
@@ -203,7 +204,7 @@ int main(){
     double M_ijk[3][3];
 
     FILE *IF;
-    
+
     IF=fopen("Disks-system.txt", "r");
     fscanf(IF,"%d",&m); fscanf(IF,"%d",&d);
 
@@ -227,37 +228,37 @@ int main(){
             double sq_dist_01=0; for(i = 0; i < d; i++){sq_dist_01 += pow(N[0][i] - N[1][i], 2);}
             double sq_dist_02=0; for(i = 0; i < d; i++){sq_dist_02 += pow(N[0][i] - N[2][i], 2);}
             double sq_dist_12=0; for(i = 0; i < d; i++){sq_dist_12 += pow(N[1][i] - N[2][i], 2);}
-            
+
             M[0][0] = 0; M[0][1] = 0; M[0][2] = N[0][d];
-            
+
             M[1][0] = sqrt(sq_dist_01); M[1][1] = 0; M[1][2] = N[1][d];
-            
+
             M[2][0] = 0.5*(sq_dist_02 + sq_dist_01 - sq_dist_12)/sqrt(sq_dist_01);
             M[2][1] = sqrt(sq_dist_02 - pow(M[2][0], 2));
             M[2][2] = N[2][d];
-            
+
             Rips_scale=0;
             for(i=0;i<2;i++){
                 for(j=i+1;j<3;j++){
                     dist_ij=0; vs dist_ij+=pow(M[i][D]-M[j][D],2); dist_ij=sqrt(dist_ij);
                     edges_scale[i][j] = dist_ij/(M[i][2]+M[j][2]);
-                    if(edges_scale[i][j] > Rips_scale){
+                    if(edges_scale[i][j] >= Rips_scale){
                         Rips_scale = edges_scale[i][j];
                         Rips_one_simplex[0] = i;
                         Rips_one_simplex[1] = j;
                     }
                 }
             }
-            
+
 
 
             // Verify if rho_M(rips_scale) >= 0.
             i = Rips_one_simplex[0]; j = Rips_one_simplex[1];
 
-            i_radious_reescaled = Rips_scale*M[i][2]; j_radious_reescaled = Rips_scale*M[j][2];
+            // i_radious_reescaled = Rips_scale*M[i][2]; j_radious_reescaled = Rips_scale*M[j][2];
             vs i_center[D] = M[i][D]; vs j_center[D] = M[j][D];
 
-            vs{ d_ij[D] = (j_radious_reescaled*i_center[D] + i_radious_reescaled*j_center[D])/(i_radious_reescaled + j_radious_reescaled);}
+            vs{ d_ij[D] = (M[j][2]*i_center[D] + M[i][2]*j_center[D])/(M[i][2] + M[j][2]);}
 
             for(D=0; D<3; D++){
                 if(D!=i && D!=j){
@@ -272,7 +273,7 @@ int main(){
                 Cech_scale = Rips_scale;
                 vs c_star[D] = d_ij[D];
 //                 printf("\n Cech scale (= Rips scale): %lf; Intersection point: (%lf,%lf)", Rips_scale, d_ij[0], d_ij[1]);
-                
+
                 ofstream output_file;
                 output_file.open("Cech_scale.txt", ios::trunc );
                 output_file << "The Cech scale agrees with the Rips scale.\n" << endl;
@@ -281,18 +282,18 @@ int main(){
                 output_file.close();
 
 //                 cout << "The Cech scale agrees with the Rips scale.\n" << endl;
-                
+
             }else{
                 Cech_scale = bisection(M, 3, Rips_scale, Rips_scale*sqrt(4.0/3), 12, 1e-12);
-                
+
                 ofstream output_file;
                 output_file.open("Cech_scale.txt", ios::trunc);
                 output_file << "The Cech scale is greater than the Rips scale.\n" << endl;
                 output_file << "Cech scale:\n" << scientific << Cech_scale << endl;
                 output_file << "\nIntersection point of the rescaled disks system:\n";
                 output_file.close();
-                
-                
+
+
                 vs i_center[D] = M[i][D]; i_radious_reescaled = Cech_scale*M[i][2];
                 vs j_center[D] = M[j][D]; j_radious_reescaled = Cech_scale*M[j][2];
                 vs k_center[D] = M[k][D]; k_radious_reescaled = Cech_scale*M[k][2];
@@ -301,7 +302,7 @@ int main(){
                 dist_ij=0; vs{ dist_ij+=pow(i_center[D]-j_center[D],2);} dist_ij=sqrt(dist_ij);
                 n_ij[0]=-c_ij[1]; n_ij[1]=c_ij[0];
 
-                // Calculate the coefficients "a" and "b" such that 
+                // Calculate the coefficients "a" and "b" such that
                 // d_ij = i_center + a * c_ij + b * n_ij and d_ji = i_center + a * c_ij - b * n_ij.
                 a = 0.5*(1 + (i_radious_reescaled - j_radious_reescaled)*(i_radious_reescaled + j_radious_reescaled)/pow(dist_ij,2));
                 b = sqrt( pow(i_radious_reescaled,2) - pow(a*dist_ij,2) )/dist_ij;
@@ -317,12 +318,12 @@ int main(){
                     vs c_star[D] = d_ji[D];
                 }
             }
-            
+
             double bc_numerator = M[2][1]*M[2][0] - (M[2][0] - M[1][0])*M[2][1];
             double bc_1 = (-1.0*M[2][1]*(c_star[0] - M[2][0]) + (M[2][0]-M[1][0])*(c_star[1]-M[2][1]))/bc_numerator;
             double bc_2 = (M[2][1]*(c_star[0] - M[2][0]) - M[2][0]*(c_star[1]-M[2][1]))/bc_numerator;
             double bc_3 = 1 - bc_1 - bc_2;
-            
+
             for(int i=0 ; i<d ; ++i){
                 p_M[i] = bc_1*N[0][i] + bc_2*N[1][i] + bc_3*N[2][i];
 
@@ -331,8 +332,8 @@ int main(){
                 output_file << scientific << p_M[i] << endl;
                 output_file.close();
             }
-            
-            
+
+
             ofstream output_file;
             output_file.open("Cech_scale.txt", std::ofstream::out | std::ofstream::app );
             output_file << "\nDimension: d="<< d << endl;
@@ -341,30 +342,30 @@ int main(){
             return 0;
         }
     }
-    
-    
+
+
 
     Rips_scale=0;
     for(i=0;i<m-1;i++){
         for(j=i+1;j<m;j++){
             dist_ij=0; vs dist_ij+=pow(M[i][D]-M[j][D],2); dist_ij=sqrt(dist_ij);
             edges_scale[i][j] = dist_ij/(M[i][2]+M[j][2]);
-            if(edges_scale[i][j] > Rips_scale){
+            if(edges_scale[i][j] >= Rips_scale){
                 Rips_scale = edges_scale[i][j];
                 Rips_one_simplex[0] = i;
                 Rips_one_simplex[1] = j;
             }
         }
     }
-    
+
     // Verify if rho_M(rips_scale) >= 0.
     i = Rips_one_simplex[0]; j = Rips_one_simplex[1];
 
-    i_radious_reescaled = Rips_scale*M[i][2]; j_radious_reescaled = Rips_scale*M[j][2];
+//    i_radious_reescaled = Rips_scale*M[i][2]; j_radious_reescaled = Rips_scale*M[j][2];
     vs i_center[D] = M[i][D]; vs j_center[D] = M[j][D];
-    
-    vs{ d_ij[D] = (j_radious_reescaled*i_center[D] + i_radious_reescaled*j_center[D])/(i_radious_reescaled + j_radious_reescaled);}
-    
+
+    vs{ d_ij[D] = (M[j][2]*i_center[D] + M[i][2]*j_center[D])/(M[i][2] + M[j][2]);}
+
     count = 0;
     for(D=0; D<m; D++){
         if(D!=i && D!=j){
@@ -372,13 +373,13 @@ int main(){
             count++;
         }
     }
-    
+
     // Suppose nonnegativity:
     ij_rho_nonnegative = 1, k_index = 0;
     while(ij_rho_nonnegative == 1 && k_index != m-2){
         k = k_values[k_index];
         vs k_center[D]=M[k][D];
-        
+
         k_radious_reescaled2 = pow(Rips_scale*M[k][2],2);
         distdkcenter=0; vs distdkcenter+=pow(d_ij[D] - k_center[D],2);
         if(k_radious_reescaled2 < distdkcenter){
@@ -386,7 +387,7 @@ int main(){
         }
         k_index++;
     }
-    
+
     // If rho_M(Rips_scale) >=0, then the Rips scale agree with the Cech scale
     if(ij_rho_nonnegative==1){
         ofstream output_file;
@@ -403,10 +404,10 @@ int main(){
 //         cout << scientific << d_ij[1] << endl;
         return 0;
     }
-    
-    
+
+
     // The following is under the value: verify_nonneg == 1
-    
+
     Cech_scale = Rips_scale;
     // We propose as Cech scale the value of the Rips scale.
     // We will update the Cech scale in each iteration:
@@ -415,14 +416,14 @@ int main(){
     for(i=0;i<m-2;i++){
         for(j=i+1;j<m-1;j++){
             ij_scale = edges_scale[i][j];
-            
+
             for(k=j+1;k<m;k++){
                 // Calculate the Rips scale of the 2-simplex[i,j,k] and assume it agree with the Cech scale: ijk_cech_scale
-                
+
                 edge_max=3;
                 if(ij_scale>=edges_scale[i][k] && ij_scale>=edges_scale[j][k] ) edge_max=1;
                 if(edges_scale[i][k]>=ij_scale && edges_scale[i][k]>=edges_scale[j][k]) edge_max=2;
-                
+
                 if(edge_max == 1){
                     for(D=0;D<3;D++){
                         D1[D]=M[i][D];
@@ -447,17 +448,17 @@ int main(){
                         ijk_cech_scale = edges_scale[j][k];
                     }
                 }
-                
+
                 // The value of ijk_cech_scale must be at least Cech_scale/sqrt(4/3):
                 if(ijk_cech_scale*sqrt(4.0/3) >= Cech_scale){
-                    
+
                     // Verify is the Rips scale of [i,j,k] agree with the Cech scale of [i,j,k].
                     vs center1[D]  = D1[D]; radious1_reescaled = ijk_cech_scale*D1[2];
                     vs center2[D]  = D2[D]; radious2_reescaled = ijk_cech_scale*D2[2];
                     vs d_ij[D] = (radious2_reescaled*center1[D] + radious1_reescaled*center2[D])/(radious1_reescaled + radious2_reescaled);
-                    
+
                     sum = sqrt( pow(D3[0]-d_ij[0], 2) + pow(D3[1]-d_ij[1], 2) );
-                    // Confirm if the Rips scale of the 2-simplex [i,j,k] agrees with the Cech scale. 
+                    // Confirm if the Rips scale of the 2-simplex [i,j,k] agrees with the Cech scale.
                     // If it does not agree, update the Cech scale using the bisection numerical method.
                     if( ijk_cech_scale*D3[2] < sum){
                         for(q=0;q<3;q++){
@@ -467,8 +468,8 @@ int main(){
                         }
                         ijk_cech_scale = bisection(M_ijk, 3, ijk_cech_scale, ijk_cech_scale*sqrt(4.0/3), 12, 1e-12);
                     }
-                    
-                    
+
+
                     // With the Cech scale of the 2-simplex [i,j,k], update the Cech scale of the disks system.
                     if(ijk_cech_scale >= Cech_scale){
                         Cech_scale = ijk_cech_scale;
@@ -481,24 +482,24 @@ int main(){
             }
         }
     }
-    
+
     i = two_simplex[0]; j = two_simplex[1]; k = two_simplex[2];
     vs i_center[D] = M[i][D]; i_radious_reescaled = two_simplex_cech_scale*M[i][2];
     vs j_center[D] = M[j][D]; j_radious_reescaled = two_simplex_cech_scale*M[j][2];
     vs k_center[D] = M[k][D]; k_radious_reescaled = two_simplex_cech_scale*M[k][2];
-    
+
     vs c_ij[D] = j_center[D] - i_center[D];
     dist_ij=0; vs{ dist_ij+=pow(i_center[D]-j_center[D],2);} dist_ij=sqrt(dist_ij);
     n_ij[0]=-c_ij[1]; n_ij[1]=c_ij[0];
-    
-    // Calculate the coefficients "a" and "b" such that 
+
+    // Calculate the coefficients "a" and "b" such that
     // d_ij = i_center + a * c_ij + b * n_ij and d_ji = i_center + a * c_ij - b * n_ij.
     a = 0.5*(1 + (i_radious_reescaled - j_radious_reescaled)*(i_radious_reescaled + j_radious_reescaled)/pow(dist_ij,2));
     b = sqrt( pow(i_radious_reescaled,2) - pow(a*dist_ij,2) )/dist_ij;
-    
+
     vs d_ij[D] = i_center[D] + a * c_ij[D] + b * n_ij[D];
     vs d_ji[D] = i_center[D] + a * c_ij[D] - b * n_ij[D];
-    
+
     sum21=0; vs sum21+=pow(k_center[D] - d_ij[D],2);
     sum22=0; vs sum22+=pow(k_center[D] - d_ji[D],2);
     if(sum21 < sum22){
@@ -515,7 +516,7 @@ int main(){
     output_file << scientific << p_M[0] << endl;
     output_file << scientific << p_M[1] << endl;
     output_file.close();
-    
+
     return 0;
 }
 
